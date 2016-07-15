86309b5c42c5318d52bb187f794627f4a39761db
sonik <sonik@DESKTOP-UN4KS64>
2016-07-16 00:27:47 +1000

    added aiChoice function

--- public_html/rps2.js
+++ public_html/rps2.js
@@ -1,1 +1,20 @@
 
+var rps = ["rock","paper","scisors"]; //global variable to minjs a bit
+
+//gets a random number and picks from 3 choices
+function aiChoice() { 
+    randNum = Math.random();
+    if (randNum <= 0.33) {
+        return rps[0];
+    }
+    else if (randNum > 0.33 && randNum < 0.67) {
+        return rps[1];
+    }
+    else {
+        return rps[2];
+    }
+    };
+
+function userChoice() {
+    //no idea how to fill this in yet
+}
